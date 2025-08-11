1fc4cbbdd4a83eb4c1c0e673d5dae5d26e4c8525
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2025-08-11 14:07:49 +0200

    core

--- src/fz_std/base/core.h
+++ src/fz_std/base/core.h
@@ -17,7 +17,14 @@
 # error Unknown trap intrinsic for this compiler.
 #endif
 
-#define Statement(S) do{ S }while(0)
+#if ASAN_ENABLED && COMPILER_MSVC
+# define no_asan __declspec(no_sanitize_address)
+#elif ASAN_ENABLED && (COMPILER_CLANG || COMPILER_GCC)
+# define no_asan __attribute__((no_sanitize("address")))
+#endif
+#if !defined(no_asan)
+# define no_asan
+#endif
 
 #if !defined(AssertBreak)
 # if defined(OS_WINDOWS)
@@ -25,6 +32,22 @@
 # else
 #  define AssertBreak(condition) (*(volatile int*)0 = 0)
 # endif
+#endif
+
+#if ASAN_ENABLED && COMPILER_MSVC
+# define no_asan __declspec(no_sanitize_address)
+#elif ASAN_ENABLED && (COMPILER_CLANG || COMPILER_GCC)
+# define no_asan __attribute__((no_sanitize("address")))
+#endif
+#if !defined(no_asan)
+# define no_asan
+#endif
+
+#if OS_WINDOWS
+# pragma section(".roglob", read)
+# define read_only no_asan __declspec(allocate(".roglob"))
+#else
+# define read_only
 #endif
 
 #if DEBUG
@@ -35,8 +58,8 @@
 # define AssertNoReentry()
 #endif
 
+#define Statement(S) do{ S }while(0)
 #define StaticAssert(condition,label) typedef u8 Glue(label,__LINE__) [(condition)?1:-1]
-
 #define ArrayCount(a) (sizeof(a)/sizeof((a)[0]))
 
 #define Min(A,B) (((A)<(B))?(A):(B))
@@ -90,39 +113,46 @@
 // Types 
 
 typedef unsigned char      u8;
-#define U8_MIN  0x00
-#define U8_MAX  0xFF
+read_only global u8 U8_MIN = 0x00;
+read_only global u8 U8_MAX = 0xFF;
+
 typedef unsigned short     u16;
-#define U16_MIN 0x0000
-#define U16_MAX 0xFFFF
+read_only global u16 U16_MIN = 0x0000;
+read_only global u16 U16_MAX = 0xFFFF;
+
 typedef unsigned int       u32;
-#define U32_MIN 0x00000000
-#define U32_MAX 0xFFFFFFFF
+read_only global u32 U32_MIN = 0x00000000;
+read_only global u32 U32_MAX = 0xFFFFFFFF;
+
 typedef unsigned long long u64;
-#define U64_MIN 0x0000000000000000
-#define U64_MAX 0xFFFFFFFFFFFFFFFF
+read_only global u64 U64_MIN = 0x0000000000000000ULL;
+read_only global u64 U64_MAX = 0xFFFFFFFFFFFFFFFFULL;
 
 typedef signed char      s8;
-#define S8_MIN  (-0x7F - 1)
-#define S8_MAX  0x7F
+read_only global s8 S8_MIN = (-0x7F - 1);
+read_only global s8 S8_MAX = 0x7F;
+
 typedef signed short     s16;
-#define S16_MIN (-0x7FFF - 1)
-#define S16_MAX 0x7FFF
+read_only global s16 S16_MIN = (-0x7FFF - 1);
+read_only global s16 S16_MAX = 0x7FFF;
+
 typedef signed int       s32;
-#define S32_MIN (-0x7FFFFFFF - 1)
-#define S32_MAX 0x7FFFFFFF
+read_only global s32 S32_MIN = (-0x7FFFFFFF - 1);
+read_only global s32 S32_MAX = 0x7FFFFFFF;
+
 typedef signed long long s64;
-#define S64_MIN (-0x7FFFFFFFFFFFFFFF - 1)
-#define S64_MAX 0x7FFFFFFFFFFFFFFF
+read_only global s64 S64_MIN = (-0x7FFFFFFFFFFFFFFFLL - 1);
+read_only global s64 S64_MAX = 0x7FFFFFFFFFFFFFFFLL;
 
 typedef float  f32;
-#define F32_MIN (-3.402823466e+38f)
-#define F32_MAX 3.402823466e+38f
-#define F32_EPSILON 1.192092896e-07f
+read_only global f32 F32_MIN     = -3.402823466e+38f;
+read_only global f32 F32_MAX     =  3.402823466e+38f;
+read_only global f32 F32_EPSILON =  1.192092896e-07f;
+
 typedef double f64;
-#define F64_MIN (-1.7976931348623158e+308)
-#define F64_MAX 1.7976931348623158e+308
-#define F64_EPSILON 2.2204460492503131e-16
+read_only global f64 F64_MIN     = -1.7976931348623158e+308;
+read_only global f64 F64_MAX     =  1.7976931348623158e+308;
+read_only global f64 F64_EPSILON =  2.2204460492503131e-16;
 
 typedef unsigned char b8;
 typedef unsigned int  b32;
