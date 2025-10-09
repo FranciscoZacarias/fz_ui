c825b2ba3e205a511d60b93f15642ef0964815df
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2025-10-09 14:28:03 +0200

    Hephaestus test

--- build.bat
+++ build.bat
@@ -1,10 +1,17 @@
 @echo off
 
+REM Run Hephaestus
+echo Running Hephaestus...
+bin\hephaestus.exe --input "../src/ui.hph"
+
+REM Build program
 set compiler_and_entry=cl ..\src\main.c
 set cl_default_flags=/Isrc /nologo /FC /Zi
 set external_include=/I"..\src\fz_std"
 
+
 if not exist build mkdir build
 pushd build
+echo Compiling...
 %compiler_and_entry% %cl_default_flags% %external_include% /Fe"fz_ui.exe"
 popd
